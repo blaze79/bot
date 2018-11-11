4e011ada7734048af16dd1ffa1387c707612a312
Vlad <blaze79@inbox.ru>
2018-11-11 20:52:31 +0600

    -change constants

--- src/main/java/org/silentpom/runner/domain/Constants.java
+++ src/main/java/org/silentpom/runner/domain/Constants.java
@@ -6,13 +6,13 @@ package org.silentpom.runner.domain;
 public class Constants {
     public static final double GOLD_COST = 100;
     public static final double REAL_GOLD_COST = 250;
-    public static final double RATE_INFLATION = 0.95;
-    public static final double VERTICAL_INFLATION = 0.98;
+    public static final double RATE_INFLATION = 0.98;
+    public static final double VERTICAL_INFLATION = 0.97;
 
-    public static final double NEAR_HUNTER = -400;
+    public static final double NEAR_HUNTER = -600;
     public static final double NEAR_HOLE_HUNTER = 30;
 
-    public static final double DEADEND_COST = -5000;
+    public static final double DEADEND_COST = -10000;
 
 
 }
